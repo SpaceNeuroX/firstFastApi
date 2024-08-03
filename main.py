5fbc97556a93ab0bc05326ba5b246a8b02e9cb4d
SpaceNeuroX <totoshkus@gmail.com>
2024-08-03 12:07:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,3 @@ recommender = Recommender()
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
-
-@app.get("/recommend")
-def get_recommendations(user_id: str):
-    # Пример получения рекомендаций
-    recommendations = recommender.recommend(user_id)
-    return {"recommendations": recommendations}
