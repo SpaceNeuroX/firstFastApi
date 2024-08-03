ba89de0a22ba46c3bf7fa481f904e943c1469e24
SpaceNeuroX <totoshkus@gmail.com>
2024-08-03 12:09:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 from fastapi import FastAPI
-from recommon import Recommender
 
 app = FastAPI()
-
-# Инициализация модели рекомендаций
-recommender = Recommender()
 
 @app.get("/")
 def read_root():
